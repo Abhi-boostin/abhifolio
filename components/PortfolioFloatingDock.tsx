9ab8c427542e02d0f7293361381954e37c8b34f7
Abhi-boostin <abhimanyusinghworks@gmail.com>
2025-07-04 02:08:56 +0530

    checkpoint

--- components/PortfolioFloatingDock.tsx
+++ components/PortfolioFloatingDock.tsx
@@ -1,14 +1,14 @@
 import { FloatingDock } from "./FloatingDock";
-import { IconHome, IconCode, IconBulb, IconBrandGithub, IconBrandLinkedin, IconDownload, IconMail } from "@tabler/icons-react";
+import { IconHome, IconCode, IconBulb, IconBrandGithub, IconBrandLinkedin, IconFileText, IconMail } from "@tabler/icons-react";
 import Link from 'next/link';
 
 const navItems = [
   { title: "Home", icon: <IconHome />, href: "/", isInternal: true },
   { title: "Projects", icon: <IconCode />, href: "/projects", isInternal: true },
   { title: "Skills", icon: <IconBulb />, href: "/skills", isInternal: true },
-  { title: "GitHub", icon: <IconBrandGithub />, href: "https://github.com/yourusername" },
-  { title: "LinkedIn", icon: <IconBrandLinkedin />, href: "https://linkedin.com/in/yourusername" },
-  { title: "Resume", icon: <IconDownload />, href: "/resume.pdf" },
+  { title: "GitHub", icon: <IconBrandGithub />, href: "https://github.com/Abhi-boostin" },
+  { title: "LinkedIn", icon: <IconBrandLinkedin />, href: "https://www.linkedin.com/in/abhiboostin/" },
+  { title: "Resume", icon: <IconFileText />, href: "https://drive.google.com/file/d/1y9pewdw7wXkRyk-TGUvp8H1_TlOW4dQB/view?usp=drive_link" },
   { title: "Mail", icon: <IconMail />, href: "mailto:your@email.com" },
 ];
 
