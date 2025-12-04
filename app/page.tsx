7500479343e2d34ca7680beb8ab983b1bf55f20f
Abhi-boostin <abhimanyusinghworks@gmail.com>
2025-12-04 15:07:43 +0530

    feat: Add initial home page component rendering HomeSection.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import PortfolioFloatingDock from "../components/PortfolioFloatingDock";
+
 import HomeSection from "../components/HomeSection";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen h-screen overflow-hidden bg-white dark:bg-neutral-950">
-      {/* Floating Dock */}
-      <div className="fixed bottom-6 left-0 right-0 z-50 flex justify-center pointer-events-none">
-        <div className="pointer-events-auto">
-          <PortfolioFloatingDock />
-        </div>
-      </div>
+
 
       {/* Home Section Only */}
       <HomeSection />
